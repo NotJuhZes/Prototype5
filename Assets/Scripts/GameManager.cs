fd828165a96bc7ac5f64f6760a6a5a8a9c811651
Joana Ferreira <jjoanagguimaraes@gmail.com>
2026-05-12 11:19:20 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+
+    void Start() StartCoroutine(SpawnTarget());
+    {
+        IEnumerator SpawnTarget()
+    
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+    void Update()
+    {
+
+    }
+
+}
+    
+
